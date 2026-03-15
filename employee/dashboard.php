bc25d5e4ba4db1f4012d37ff4fc1917aced25fc6
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:47:58 +0300

    Update dashboard.php

--- employee/dashboard.php
+++ employee/dashboard.php
@@ -1,13 +1,13 @@
 <?php
-require_once '../config/database.php';
 
-if(!isset($_SESSION['user_id'])){
-header("Location: ../login.php");
+require '../config/database.php';
+
+if($_SESSION['role']!="Employee"){
+header("Location:../login.php");
 }
+
 ?>
 
 <h1>Employee Dashboard</h1>
-
-<a href="create_request.php">Create Request</a>
 
 <a href="../logout.php">Logout</a>
