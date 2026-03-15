d412425ccfd0acbb4fa5615324247d50bf7007f0
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:09:33 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,33 +1,49 @@
-CREATE DATABASE IF NOT EXISTS key_it_maintenance;
+CREATE DATABASE key_it_maintenance;
+
 USE key_it_maintenance;
 
-CREATE TABLE users (
+CREATE TABLE users(
 id INT AUTO_INCREMENT PRIMARY KEY,
-full_name VARCHAR(100),
+name VARCHAR(100),
 email VARCHAR(100) UNIQUE,
+department VARCHAR(100),
 password VARCHAR(255),
-department VARCHAR(50),
-role ENUM('Admin','Employee','Technician') DEFAULT 'Employee',
+role ENUM('Admin','Employee','Technician'),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE requests (
+CREATE TABLE requests(
 id INT AUTO_INCREMENT PRIMARY KEY,
-ticket_id VARCHAR(20),
 user_id INT,
-request_type VARCHAR(50),
-category VARCHAR(100),
+type VARCHAR(50),
+description TEXT,
 priority VARCHAR(20),
 status VARCHAR(20) DEFAULT 'Pending',
-description TEXT,
+technician_id INT,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users(full_name,email,password,department,role)
+CREATE TABLE request_comments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+request_id INT,
+user_id INT,
+comment TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE notifications(
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+message TEXT,
+is_read INT DEFAULT 0,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users(name,email,department,password,role)
 VALUES(
 'System Admin',
 'admin@keycompany.com',
-'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
 'IT',
+'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
 'Admin'
 );
