827f51809fd9dd47b524f5e86d6fbb86443f2276
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:46:59 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,15 +1,6 @@
 <?php
 
-require '../config/database.php';
+session_start();
+session_destroy();
 
-if(!isset($_SESSION['user_id'])){
-
-header("Location:../login.php");
-
-}
-
-?>
-
-<h1>Employee Dashboard</h1>
-
-<a href="../logout.php">Logout</a>
+header("Location: index.php");
