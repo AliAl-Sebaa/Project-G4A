a26b2da3e40e478ed52d42ded21c82d0db31d557
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:44:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,17 +14,27 @@ href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
 <body>
 
-<div class="hero">
+<section class="hero">
 
-<h1>Key Company IT Maintenance System</h1>
+<div class="container text-center">
 
-<p>Smart platform for managing IT maintenance requests.</p>
+<h1 class="display-4">Key Company IT Maintenance System</h1>
 
-<a href="login.php" class="btn btn-primary">Login</a>
+<p class="lead">
+Smart platform for managing IT maintenance and technical support requests.
+</p>
 
-<a href="register.php" class="btn btn-success">Create Account</a>
+<a href="login.php" class="btn btn-primary btn-lg">
+Login
+</a>
+
+<a href="register.php" class="btn btn-success btn-lg">
+Create Account
+</a>
 
 </div>
+
+</section>
 
 </body>
 
