ebb64bab0c405bc0b6c8a5768921667ab458be16
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:20:02 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,31 +1,43 @@
 <?php
-require_once 'config/database.php';
 
-if($_SERVER['REQUEST_METHOD']=="POST"){
+require 'config/database.php';
+
+if($_POST){
 
 $name=$_POST['name'];
 $email=$_POST['email'];
-$pass=password_hash($_POST['password'],PASSWORD_DEFAULT);
+$department=$_POST['department'];
+$password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+$role=$_POST['role'];
 
-$stmt=$pdo->prepare("INSERT INTO users(full_name,email,password) VALUES(?,?,?)");
+$stmt=$pdo->prepare("INSERT INTO users(name,email,department,password,role)
+VALUES(?,?,?,?,?)");
 
-$stmt->execute([$name,$email,$pass]);
+$stmt->execute([$name,$email,$department,$password,$role]);
 
-header("Location: login.php");
+header("Location:login.php");
 
 }
+
 ?>
 
 <form method="POST">
 
-<h2>Create Account</h2>
-
-<input name="name" placeholder="Name">
+<input name="name" placeholder="Full Name">
 
 <input name="email" placeholder="Email">
 
-<input name="password" type="password" placeholder="Password">
+<input name="department" placeholder="Department">
 
-<button>Register</button>
+<input type="password" name="password">
+
+<select name="role">
+
+<option>Employee</option>
+<option>Technician</option>
+
+</select>
+
+<button type="submit">Register</button>
 
 </form>
