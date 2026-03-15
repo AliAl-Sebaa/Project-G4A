391901fea91adafc09d47236e492b83c08bf6d25
AliAl-Sebaa <Alimseba716@gmail.com>
2026-03-15 12:18:42 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,41 @@
 <?php
-require_once 'config/database.php';
 
-if($_SERVER['REQUEST_METHOD']=="POST"){
+require 'config/database.php';
 
-$email=$_POST['email'];
-$password=$_POST['password'];
+if($_POST){
 
-$stmt=$pdo->prepare("SELECT * FROM users WHERE email=?");
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE email=?");
+
 $stmt->execute([$email]);
 
-$user=$stmt->fetch();
+$user = $stmt->fetch();
 
 if($user && password_verify($password,$user['password'])){
 
 $_SESSION['user_id']=$user['id'];
 $_SESSION['role']=$user['role'];
 
-header("Location: employee/dashboard.php");
+if($user['role']=="Admin") header("Location: admin/dashboard.php");
 
-}else{
-$error="Wrong login";
+if($user['role']=="Employee") header("Location: employee/dashboard.php");
+
+if($user['role']=="Technician") header("Location: technician/dashboard.php");
+
 }
 
 }
+
 ?>
 
 <form method="POST">
 
-<h2>Login</h2>
-
-<?php if(isset($error)) echo $error; ?>
-
-<input name="email" placeholder="Email">
+<input type="email" name="email" placeholder="Email">
 
 <input type="password" name="password" placeholder="Password">
 
-<button>Login</button>
+<button type="submit">Login</button>
 
 </form>
